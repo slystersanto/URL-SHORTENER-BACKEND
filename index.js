ed217a3211399f05bfe066055bd903468386c2f2
slystersanto <santhosh.mech.19@gmail.com>
2023-04-27 15:30:25 +0530

    stage_commit

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const bcryptjs = require('bcryptjs');
 const moment = require('moment');
 app.use(express.json());
 app.use(cors());
-app.use(
-    cors({
-      origin: "http://localhost:3000",
-    })
-  );
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // allow requests from any origin
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any origin
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
