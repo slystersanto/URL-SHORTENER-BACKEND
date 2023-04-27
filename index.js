65af9ea6eccafe368af75c300e8bcdd8e8eeb50c
slystersanto <santhosh.mech.19@gmail.com>
2023-04-27 16:44:51 +0530

    last

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const moment = require('moment');
 app.use(express.json());
 app.use(cors());
 
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any origin
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+   
+  res.setHeader('Access-Control-Allow-Origin', 'https://chimerical-brigadeiros-3f98f6.netlify.app/','http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
