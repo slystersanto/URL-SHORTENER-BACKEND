d67fab194b0140fc01ad290160549c7ec513fe41
slystersanto <santhosh.mech.19@gmail.com>
2023-04-27 16:48:48 +0530

    last

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
    
-  res.setHeader('Access-Control-Allow-Origin', 'https://chimerical-brigadeiros-3f98f6.netlify.app/','http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://chimerical-brigadeiros-3f98f6.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
